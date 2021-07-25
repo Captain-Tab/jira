dddc5c46c28c1b926f7a6ed51619774455e6673c
captain-tab <d491405879@gmail.com>
2021-07-25 23:52:23 +0800

    feat: create custom hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,7 +1,7 @@
 import { SearchPanel } from "./search";
 import {List} from "./list";
 import React, { useEffect, useState } from "react";
-import { cleanObject } from "../../utils";
+import { cleanObject, useDebounce, useMount } from "../../utils";
 import * as qs from "qs";
 
 const apiUrl = process.env["REACT_APP_API_URL"]
@@ -13,22 +13,23 @@ export const ProjectListScreen = ()=> {
   })
   const [list, setList] = useState([])
   const [users, setUsers] = useState([])
+  const debounceParam = useDebounce(param, 200)
 
   useEffect(()=> {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response=> {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async response=> {
       if (response.ok) {
         setList(await response.json())
       }
     })
-  },[param])
+  },[debounceParam])
 
-  useEffect(()=> {
+  useMount(()=> {
     fetch(`${apiUrl}/users`).then(async response=> {
       if (response.ok) {
         setUsers(await response.json())
       }
     })
-  },[])
+  })
 
   return <div>
     <SearchPanel users={users} param={param} setParam={setParam}/>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,10 @@
+// identify the falsy value
+import { useEffect, useState } from "react";
+import { set } from "husky/lib";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
-//
+
+// clean the useless value of object
 export const cleanObject = (object) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
@@ -9,4 +14,23 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+
+// create custom mounted hook
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+// create custom debounce hook
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => setDebounceValue(value), delay);
+    return () => clearTimeout(timeOut);
+  }, [value, delay]);
+
+  return debounceValue;
 };
