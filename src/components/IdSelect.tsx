00730544e5bd08f2032200e47a77c730f9d5fe2e
captain-tab <d491405879@gmail.com>
2021-10-05 22:18:22 +0800

    feat: add IdSelcet component

---
+++ src/components/IdSelect.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Raw } from "../../type";
+import { Select } from "antd";
+
+type SelectProps = React.ComponentProps<typeof Select>;
+
+interface IdSelectProps
+  extends Omit<SelectProps, "value" | "onChange" | "options"> {
+  value: Raw | null | undefined;
+  onChange: (value?: number) => void;
+  defaultOptionName?: string;
+  options?: {
+    name: string;
+    id: number;
+  }[];
+}
+
+/**
+ *  value could be different type
+ *  onChange only receive number or undefined type
+ *  it is will choose default type when isNaN(value) === true
+ *  Once choose default type, onChange will use undefined type
+ * */
+export const IdSelect = (props: IdSelectProps) => {
+  const { value, onChange, defaultOptionName, options, ...restProps } = props;
+  return (
+    <Select
+      value={toNumber(value)}
+      onChange={(value) => onChange(toNumber(value) || undefined)}
+      {...restProps}
+    >
+      {defaultOptionName ? (
+        <Select.Option value={0}>{defaultOptionName}</Select.Option>
+      ) : null}
+      {options?.map((option) => (
+        <Select.Option value={option.id} key={option.id}>
+          {option.name}
+        </Select.Option>
+      ))}
+    </Select>
+  );
+};
+
+const toNumber = (value: unknown) => (isNaN(Number(value)) ? 0 : Number(value));
