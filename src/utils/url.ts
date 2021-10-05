71104751bf1bc8be3ca7518ec1836d42aa48858e
captain-tab <d491405879@gmail.com>
2021-10-05 17:31:46 +0800

    feat: create useUrlQueyParam

---
+++ src/utils/url.ts
@@ -0,0 +1,14 @@
+/**
+ * return value from url query object
+ */
+import { useSearchParams } from "react-router-dom";
+
+export const useUrlQueryParam = (keys: string[]) => {
+  const [searchParams, setSearchParam] = useSearchParams();
+  return [
+    keys.reduce((prev, key) => {
+      return { ...prev, [key]: searchParams.get(key) || "" };
+    }, {} as { [key in string]: string }),
+    setSearchParam,
+  ] as const;
+};
